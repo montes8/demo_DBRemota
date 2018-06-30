44b7a6e847b985a802a98487d6248dbc8e30894a
montes8 <emontesinos812@gmail.com>
2018-06-30 18:57:44 -0500

    registrando usuario en la base de datos

--- app/src/main/java/com/example/thais/demowebservice/fragments/DesarrolladorFragment.kt
+++ app/src/main/java/com/example/thais/demowebservice/fragments/DesarrolladorFragment.kt
@@ -9,14 +9,16 @@ import android.view.View
 import android.view.ViewGroup
 import com.android.volley.Response
 import com.android.volley.VolleyError
-
 import com.example.thais.demowebservice.R
 import org.json.JSONObject
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import android.app.ProgressDialog
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.fragment_registrar_usuario.*
 
 
 class DesarrolladorFragment : Fragment(), Response.Listener<JSONObject>,Response.ErrorListener {
@@ -48,13 +50,31 @@ class DesarrolladorFragment : Fragment(), Response.Listener<JSONObject>,Response
     }
     override fun onResponse(response: JSONObject?) {
 
+        Toast.makeText(getContext(),"se ha registrado correctamente",Toast.LENGTH_LONG).show()
+       progreso?.hide()
+        editText_documento.setText("")
+        editText_nombre.setText("")
+        editText_profesion.setText("")
     }
 
     override fun onErrorResponse(error: VolleyError?) {
 
+        progreso?.hide()
+        Toast.makeText(getContext(),"se ha registrado correctamente"+error.toString(),Toast.LENGTH_LONG).show()
+        Log.d("error",error.toString())
     }
 
     fun cargarWebService(){
-        
+
+        progreso = ProgressDialog(getContext())
+        progreso?.setMessage("cargando ...")
+        progreso?.show()
+
+        var url = "http://192.168.137.1/ejemploBDRemota/JSONRegistro.php?documento="+editText_documento.getText().toString()+"&nombre="+editText_nombre.getText().toString()+"&profesion="+editText_profesion.getText().toString()
+
+        url=url.replace(" ","%20")
+
+        jsonObjectRequest = JsonObjectRequest(Request.Method.GET,url,null,this,this)
+        request?.add(jsonObjectRequest)
     }
 }
