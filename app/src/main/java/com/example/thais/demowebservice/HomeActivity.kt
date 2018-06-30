27ef48ae45ee809fd1c85405eeafa2004bb42216
montes8 <emontesinos812@gmail.com>
2018-06-30 17:26:46 -0500

    navegacion de fragment terminado

--- app/src/main/java/com/example/thais/demowebservice/HomeActivity.kt
+++ app/src/main/java/com/example/thais/demowebservice/HomeActivity.kt
@@ -1,40 +1,94 @@
 package com.example.thais.demowebservice
 
-import android.net.Uri
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
-import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
-import com.example.thais.demowebservice.fragments.*
-import com.example.thais.demowebservice.interfaces.IFragments
+import com.example.thais.demowebservice.fragments.BienvenidaFragment
+import com.example.thais.demowebservice.fragments.DesarrolladorFragment
+import com.example.thais.demowebservice.fragments.RegistrarUsuarioFragment
+import kotlinx.android.synthetic.main.activity_home.*
+
 
 class HomeActivity : AppCompatActivity() {
 
-    var drawerLayout : DrawerLayout? = null
-    var navigationView : NavigationView? = null
+    lateinit var drawerLayout : DrawerLayout
+    lateinit var navigationView : NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        drawerLayout = findViewById(R.id.drawer_layout)
-        navigationView = findViewById(R.id.nav_view)
+        drawerLayout = findViewById(R.id.drawer_layout)//traemos la vista de nuestro draerLayout
+        navigationView = findViewById(R.id.navigationView)
 
-
-
-
-
-
+        setSupportActionBar(mToolbar_home)
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_navigation)//definimco que imagen queremos llamr la imagen
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)//habilitamos el icono
+        cambiarFragmento(RegistrarUsuarioFragment(),navigationView?.menu.getItem(0))
+        navegacionDrawer()
     }
 
 
+    //registramos evento navigationDrawer
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                drawerLayout!!.openDrawer(GravityCompat.START)
+                return true
+            }
 
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun cambiarFragmento(fragment: Fragment?, item:MenuItem){
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.content_frame,fragment)
+                .commit()
+        item.isChecked = true
+        supportActionBar!!.title = item.title
+    }
+
+    fun navegacionDrawer(){
+        navigationView.setNavigationItemSelectedListener { item ->
+            var gestorDeFragmentos = false
+            var fragment : Fragment? = null
+            when (item.itemId) {
+                R.id.nav_inicio -> {
+                    fragment = BienvenidaFragment()
+                    gestorDeFragmentos = true
+                }
+                R.id.nav_registro -> {
+                    fragment = RegistrarUsuarioFragment()
+                    gestorDeFragmentos = true
+                }
+                R.id.nav_desarrollador -> {
+                   fragment = DesarrolladorFragment()
+                    gestorDeFragmentos = true
+                }
+
+            }
+            if (gestorDeFragmentos){
+                cambiarFragmento(fragment,item)
+                drawerLayout?.closeDrawers()
+            }
+            true
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
 
 
 }
