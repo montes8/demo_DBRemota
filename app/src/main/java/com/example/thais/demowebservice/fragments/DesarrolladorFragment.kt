a4024d78aa4f277792f56c32052a2edb7aeab2c0
montes8 <emontesinos812@gmail.com>
2018-06-30 17:59:18 -0500

    cargando las ferencias de los view en la clase registrar

--- app/src/main/java/com/example/thais/demowebservice/fragments/DesarrolladorFragment.kt
+++ app/src/main/java/com/example/thais/demowebservice/fragments/DesarrolladorFragment.kt
@@ -1,34 +1,60 @@
 package com.example.thais.demowebservice.fragments
 
 
-import android.net.Uri
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.android.volley.Response
+import com.android.volley.VolleyError
 
 import com.example.thais.demowebservice.R
+import org.json.JSONObject
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import android.app.ProgressDialog
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
-class DesarrolladorFragment : Fragment() {
+class DesarrolladorFragment : Fragment(), Response.Listener<JSONObject>,Response.ErrorListener {
 
+    @Suppress("DEPRECATION")
+    var progreso: ProgressDialog? = null
+    var jsonObjectRequest: JsonObjectRequest? = null
+    var request: RequestQueue? = null
+    var miVista : View? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_desarrollador, container, false)
+        miVista = inflater.inflate(R.layout.fragment_desarrollador, container, false)
+
+        val editDocumento = miVista?.findViewById<View>(R.id.editText_documento)
+        val editNombre = miVista?.findViewById<View>(R.id.editText_nombre)
+        val editProfecion = miVista?.findViewById<View>(R.id.idProfesion)
+        val btnRegistrar = miVista?.findViewById<View>(R.id.btn_registrarusuario)
+
+
+       request = Volley.newRequestQueue(getContext())
+
+
+        btnRegistrar?.setOnClickListener {
+
+            cargarWebService()
+        }
+
+        return miVista
+    }
+    override fun onResponse(response: JSONObject?) {
+
     }
 
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
+    override fun onErrorResponse(error: VolleyError?) {
+
+    }
+
+    fun cargarWebService(){
+        
     }
 }
