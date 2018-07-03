6a1411884801a4595a5648a3ca45736e82093839
emontesinos812 <emontesinos812@gmail.com>
2018-07-03 06:57:13 -0300

    registrando producto 2da forma

--- app/src/main/java/com/example/thais/demowebservice/DemoActivity.kt
+++ app/src/main/java/com/example/thais/demowebservice/DemoActivity.kt
@@ -3,7 +3,10 @@ package com.example.thais.demowebservice
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import com.example.thais.demowebservice.R.id.edit_id_demo
 import com.example.thais.demowebservice.entidades.Producto
 import kotlinx.android.synthetic.main.activity_demo.*
 import retrofit2.Call
@@ -32,24 +35,29 @@ class DemoActivity : AppCompatActivity() {
             }
         })
 
-        btn_registrardemo?.setOnClickListener {
-            val p = Producto(11, "phone hawei", 1000.00, 10, 10, "nuevo phone G8")
+        btn_registrardemo.setOnClickListener {
+            val p = Producto(10,"phone hawe", 1001.00, 12, 12, "nuevo phone G8")
             val mandardatos = galaxyService.registrar(p)
-            mandardatos.enqueue(object : Callback<Producto> {
-                override fun onResponse(call: Call<Producto>?, response: Response<Producto>?) {
-                    val respuesta = response?.body()
-                    Toast.makeText(this@DemoActivity, "producto registrado", Toast.LENGTH_LONG).show()
+
+            mandardatos.enqueue(object : Callback<Void> {
+                override fun onResponse(call: Call<Void>, response: Response<Void>) {
+
+                    Toast.makeText(this@DemoActivity, "producto registrado ${response.code()}", Toast.LENGTH_LONG).show()
+
                 }
 
-                override fun onFailure(call: Call<Producto>?, t: Throwable?) {
-
+                override fun onFailure(call: Call<Void>, t: Throwable) {
+                    Toast.makeText(this@DemoActivity, "producto no registrado", Toast.LENGTH_LONG).show()
+                    Log.d("errorregistar",""+t.toString())
                 }
             })
         }
             button_buscar.setOnClickListener {
 
 
-                val buscarProducto = galaxyService.obtenerProductoSegunId(6)
+
+
+                val buscarProducto = galaxyService.obtenerProductoSegunId(edit_id_demo.text.toString().toInt())
                 buscarProducto.enqueue(object : Callback<Producto> {
                     override fun onResponse(call: Call<Producto>?, response: Response<Producto>?) {
                         val producto = response?.body()
@@ -67,7 +75,7 @@ class DemoActivity : AppCompatActivity() {
 
                 button_eliminar.setOnClickListener{
 
-                    val eliminarProducto = galaxyService.eliminar(10)
+                    val eliminarProducto = galaxyService.eliminar(5)
 
                     eliminarProducto.enqueue(object :Callback<Producto>{
                         override fun onResponse(call: Call<Producto>?, response: Response<Producto>?) {
@@ -75,7 +83,7 @@ class DemoActivity : AppCompatActivity() {
                         }
 
                         override fun onFailure(call: Call<Producto>?, t: Throwable?) {
-                            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
                         }
                     })
 
